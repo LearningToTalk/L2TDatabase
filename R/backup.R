4c87eda6c1e014485237568a29dd1805028287a9
unknown <mahr@wisc.edu>
2015-07-10 10:36:30 -0500

    complete documentation for backup.R

--- R/backup.R
+++ R/backup.R
@@ -1,19 +1,12 @@
-#' Backup each tbl in the L2T database
-#' @export
-l2t_backup <- function(l2t_con, backup_dir) {
-  # Create a folder for today's date
-  stamp <- format(Sys.time(), "%Y-%m-%d_%H-%M")
-  this_backup_dir <- file.path(backup_dir, stamp)
-  dir.create(this_backup_dir, showWarnings = FALSE, recursive = TRUE)
 
-  # Backup each tbl in the database connection
-  tbls <- src_tbls(l2t_con)
-  dfs <- lapply(tbls, backup_tbl, src = l2t_con, output_dir = this_backup_dir)
-  names(dfs) <- tbls
-  dfs
-}
 
 #' Download a tbl from a db connection and write to a csv
+#' @param tbl_name the name of a database table
+#' @param src a dplyr-managed connection to a database
+#' @param output_dir directory where the csv is to be saved
+#' @return A copy of the table is downloaded from the database and returned. A
+#'   blank data-frame is returned if the table could not be downloaded. The
+#'   contents of the table are written to \code{[output_dir]/[tbl_name].csv}.
 #' @export
 backup_tbl <- function(tbl_name, src, output_dir) {
   # Try to download the tbl, defaulting to an empty data-frame
@@ -26,9 +19,35 @@ backup_tbl <- function(tbl_name, src, output_dir) {
   df
 }
 
+#' Download each tbl in a database and write to csv
+#' @inheritParams backup_tbl
+#' @param backup_dir location where to save the backed up table
+#' @return Each table in the database is downloaded and saved via
+#'   \code{backup_tbl}. The downloaded tables are returned in a list of
+#'   dataframes. The csvs are saved together in a timestamped folder inside of
+#'   the \code{backup_dir} folder, so the path to a particular's table's csv
+#'   file would be \code{[backup_dir]/[timestamp]/[table_name].csv}.
+#' @export
+l2t_backup <- function(src, backup_dir) {
+  # Create a folder for today's date
+  stamp <- format(Sys.time(), "%Y-%m-%d_%H-%M")
+  this_backup_dir <- file.path(backup_dir, stamp)
+  dir.create(this_backup_dir, showWarnings = FALSE, recursive = TRUE)
+
+  # Backup each tbl in the database connection
+  tbls <- src_tbls(src)
+  dfs <- lapply(tbls, backup_tbl, src = src, output_dir = this_backup_dir)
+  names(dfs) <- tbls
+  dfs
+}
 
 
 #' Download a codebook for a database table
+#' @inheritParams backup_tbl
+#' @return A dataframe describing each column of a database table. Columns in
+#'   the dataframe: Table, Field, Index (type of database index), (storage)
+#'   DataType, DefaultValue, NullAllowed (whether blanks are allowed),
+#'   Description (the comment field from the table structure).
 #' @export
 #' @importFrom DBI dbGetQuery
 describe_tbl <- function(src, tbl_name) {
@@ -52,7 +71,11 @@ describe_tbl <- function(src, tbl_name) {
 }
 
 
-#' Download a codebook for a database connection
+#' Download a codebook for a database
+#' @inheritParams backup_tbl
+#' @return A dataframe describing each table in a database. Columns in the
+#'   dataframe: Database, Table, (number of) Rows, Description (comment field
+#'   for the table)
 #' @export
 #' @importFrom DBI dbGetQuery
 describe_db <- function(src) {
@@ -72,3 +95,4 @@ describe_db <- function(src) {
 
   info
 }
+
