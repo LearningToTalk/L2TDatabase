8a7ac106d7ca456e44d3e917f355a5168bc96e32
unknown <mahr@wisc.edu>
2015-08-20 15:09:01 -0500

    seed SES table

---
+++ inst/migrations/ses/seed.R
@@ -0,0 +1,19 @@
+# Seed the SES table with children
+library("L2TDatabase")
+library("dplyr")
+
+# Download/backup db beforehand
+cnf_file <- file.path(getwd(), "inst/l2t_db.cnf")
+l2t <- l2t_connect(cnf_file)
+l2t_dl <- l2t_backup(l2t, "inst/backup")
+
+# Find children not yet in SES table
+new_children <- anti_join(l2t_dl$Child, l2t_dl$SES) %>%
+  select(ChildID) %>%
+  mutate(SES_Notes = "Entry seeded. Need to enter data")
+
+# Update the remote table
+l2t_write <- l2t_writer_connect("inst/l2t_db.cnf", "l2t")
+
+# An error here is a good thing if there are no new rows to add
+append_rows_to_table(l2t_write, "SES", new_children)
