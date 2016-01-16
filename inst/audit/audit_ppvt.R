4f37dc02c4e76c966853aa4af6125ff7902bc16f
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-01-16 12:53:52 -0600

    add *real* ppvt audit script

--- inst/audit/audit_ppvt.R
+++ inst/audit/audit_ppvt.R
@@ -1,4 +1,4 @@
-# Check for inconsistencies in the EVT scores in the database
+# Check for inconsistencies in the PPVT scores in the database
 
 library("L2TDatabase")
 library("dplyr")
@@ -14,59 +14,59 @@ cds <- l2t_dl$ChildStudy %>%
   left_join(l2t_dl$Child)
 cds
 
-evts <- left_join(cds, l2t_dl$EVT) %>%
-  select(Study, ID = ShortResearchID, EVT_Form:EVT_Age)
+ppvts <- left_join(cds, l2t_dl$PPVT) %>%
+  select(Study, ID = ShortResearchID, PPVT_Form:PPVT_Age)
 
-with_scores <- evts %>%
-  filter(!is.na(EVT_Raw))
+with_scores <- ppvts %>%
+  filter(!is.na(PPVT_Raw))
 
 missing_dates <- with_scores %>%
-  filter(is.na(EVT_Completion))
+  filter(is.na(PPVT_Completion))
 missing_dates
 
 missing_forms <- with_scores %>%
-  filter(is.na(EVT_Form)) %>%
+  filter(is.na(PPVT_Form)) %>%
   arrange(Study, ID)
 
 as.data.frame(missing_forms)
 
 # Get dates and ages
-ages <- left_join(cds, l2t_dl$EVT) %>%
-  select(Study, ShortResearchID, Birthdate, EVT_Completion, EVT_Age) %>%
-  filter(!is.na(EVT_Completion))
+ages <- left_join(cds, l2t_dl$PPVT) %>%
+  select(Study, ShortResearchID, Birthdate, PPVT_Completion, PPVT_Age) %>%
+  filter(!is.na(PPVT_Completion))
 
 # Compute chronological age
 ages <- ages %>%
   rowwise %>%
-  mutate(ChronoAge = chrono_age(Birthdate, EVT_Completion)) %>%
+  mutate(ChronoAge = chrono_age(Birthdate, PPVT_Completion)) %>%
   ungroup
 
-ages %>% filter(EVT_Age != ChronoAge)
+ages %>% filter(PPVT_Age != ChronoAge)
 
 
 
 
 # Check scores against norms
-evt_norms <- l2t_connect(cnf_file, "norms") %>% tbl("EVT2") %>% collect
+ppvt_norms <- l2t_connect(cnf_file, "norms") %>% tbl("PPVT4") %>% collect
 
 # Make a form of the table amenable for score checking
 norm_check <- with_scores %>%
   # Round age up to 30 months if younger than test norms
-  mutate(Age = ifelse(EVT_Age < 30, 30, EVT_Age)) %>%
-  rename(Raw = EVT_Raw, TestAge = EVT_Age,
-         OurStnd = EVT_Standard, OurGSV = EVT_GSV)
-
-# Get norms for each Age, Raw Score
-form_a <- norm_check %>%
-  filter(EVT_Form %in% "A") %>%
-  left_join(evt_norms) %>%
-  rename() %>%
-  select(Study:EVT_Completion, TestAge, NormAge = Age, AgePage,
-         Raw, OurStnd, Stnd, OurGSV, GSV)
+  mutate(Age = ifelse(PPVT_Age < 30, 30, PPVT_Age)) %>%
+  rename(Raw = PPVT_Raw, TestAge = PPVT_Age,
+         OurStnd = PPVT_Standard, OurGSV = PPVT_GSV)
 
 # Look for Form Bs
 norm_check %>%
-  filter(EVT_Form == "B")
+  filter(PPVT_Form == "B")
+
+# Get norms for each Age, Raw Score
+form_a <- norm_check %>%
+  filter(PPVT_Form %in% "A") %>%
+  left_join(ppvt_norms) %>%
+  rename() %>%
+  select(Study:PPVT_Completion, TestAge, NormAge = Age, AgePage,
+         Raw, OurStnd, Stnd, OurGSV, GSV)
 
 # Check GSVs
 form_a %>% filter(OurGSV != GSV)
@@ -76,10 +76,10 @@ form_a %>% filter(OurStnd != Stnd) %>% arrange(Study, ID) %>% as.data.frame
 
 # Try to checks score where we don't know the form
 form_na <- norm_check %>%
-  filter(is.na(EVT_Form)) %>%
-  left_join(evt_norms) %>%
+  filter(is.na(PPVT_Form)) %>%
+  left_join(ppvt_norms) %>%
   rename() %>%
-  select(Study:EVT_Completion, TestAge, NormAge = Age, AgePage,
+  select(Study:PPVT_Completion, TestAge, NormAge = Age, AgePage,
          Raw, OurStnd, Stnd, OurGSV, GSV)
 
 # Check GSVs
