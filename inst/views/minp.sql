c3d9353431d3383df07d9582e099d8651a206f44
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-03-11 14:21:31 -0600

    create min pair view. closes #23

---
+++ inst/views/minp.sql
@@ -0,0 +1,16 @@
+--
+-- Create a view to display the proportion correct for non-training trials in
+-- each administration of the Minimal Pairs experiment.
+--
+
+CREATE ALGORITHM = UNDEFINED VIEW  `q_MinPair_Aggregate` AS SELECT c.Study, d.ShortResearchID AS  `ResearchID` , b.MinPair_EprimeFile, b.MinPair_Completion, b.MinPair_Dialect, ROUND( AVG( a.Correct ) , 4.0 ) AS  `MinPair_ProportionCorrect`
+FROM ChildStudy d
+LEFT JOIN Study c
+USING ( StudyID )
+LEFT JOIN MinPair_Admin b
+USING ( ChildStudyID )
+LEFT JOIN MinPair_Responses a
+USING ( MinPairID )
+WHERE a.Running =  "Test"
+GROUP BY b.MinPairID
+ORDER BY b.MinPairID
