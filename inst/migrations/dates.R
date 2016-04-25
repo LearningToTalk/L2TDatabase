5abd372d9a461f3e8b8d6b8e6cc3e378eb189e07
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-04-25 08:57:25 -0500

    let DIRT spreadsheet reducer search subdirectories

--- inst/migrations/dates.R
+++ inst/migrations/dates.R
@@ -1,13 +1,14 @@
+# Load and reduce all the DIRT spreadsheets
 library("stringr")
 library("readxl")
 library("dplyr")
 library("tidyr")
 
 # get all the date data from a single directory of data spreadsheets
-collect_dates <- function(date_dir) {
+collect_dates <- function(date_dir, pattern = "TimePoint.Dates", recursive = FALSE) {
   # get the full path of each spreadsheet
   date_spreadsheets <- date_dir %>%
-    list.files("Dates", full.names = TRUE) %>%
+    list.files(pattern = pattern, full.names = TRUE, recursive = recursive) %>%
     normalizePath(mustWork = TRUE)
 
   # get the contents of each spreadsheet in large dataframe
