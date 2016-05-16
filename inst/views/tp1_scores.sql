6a0ffef7f1198a7b297495b9c093beb6811d1549
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-05-16 08:34:57 -0500

    add vf to queries

--- inst/views/tp1_scores.sql
+++ inst/views/tp1_scores.sql
@@ -1,8 +1,9 @@
 --
 -- Create a view to display the test scores of TimePoint1 participants
+-- Replace CREATE with ALTER to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint1` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID` , c.Female, c.AAE, c.LateTalker, c.CImplant, c.Birthdate, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard, g.GFTA_Completion, g.GFTA_RawCorrect, g.GFTA_NumTranscribed, g.GFTA_AdjCorrect, g.GFTA_Standard
+CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint1` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID`, c.Female, c.AAE, c.LateTalker, c.CImplant, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard, f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq, g.GFTA_Completion, g.GFTA_RawCorrect, g.GFTA_NumTranscribed, g.GFTA_AdjCorrect, g.GFTA_Standard
 FROM Study a
 LEFT JOIN ChildStudy b
 USING ( StudyID )
@@ -11,6 +12,8 @@ USING ( ChildID )
 LEFT JOIN EVT d
 USING ( ChildStudyID )
 LEFT JOIN PPVT e
+USING ( ChildStudyID )
+LEFT JOIN VerbalFluency f
 USING ( ChildStudyID )
 LEFT JOIN GFTA g
 USING ( ChildStudyID )
--- inst/views/tp3_scores.sql
+++ inst/views/tp3_scores.sql
@@ -1,8 +1,9 @@
 --
 -- Create a view to display the test scores of TimePoint3 participants
+-- Replace CREATE with ALTER to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint3` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID` , c.Female, c.AAE, c.LateTalker, c.CImplant, c.Birthdate, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard
+CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint3` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID` , c.Female, c.AAE, c.LateTalker, c.CImplant, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard, f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq
 FROM Study a
 LEFT JOIN ChildStudy b
 USING ( StudyID )
@@ -11,6 +12,8 @@ USING ( ChildID )
 LEFT JOIN EVT d
 USING ( ChildStudyID )
 LEFT JOIN PPVT e
+USING ( ChildStudyID )
+LEFT JOIN VerbalFluency f
 USING ( ChildStudyID )
 WHERE a.Study =  "TimePoint3"
 ORDER BY b.ShortResearchID
