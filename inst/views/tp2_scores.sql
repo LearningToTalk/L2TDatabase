00de972a00b1cecf90dd1b7dfb0afaa88e2fc952
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-06-22 14:17:04 -0500

    add SAILS to tp2 query

--- inst/views/tp2_scores.sql
+++ inst/views/tp2_scores.sql
@@ -3,19 +3,31 @@
 -- Replace CREATE with ALTER to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint2` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID` , c.Female, c.AAE, c.LateTalker, c.CImplant, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard, f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq, h.FruitStroop_Completion, h.FruitStroop_Age, h.FruitStroop_Score
-FROM Study a
-LEFT JOIN ChildStudy b
-USING ( StudyID )
-LEFT JOIN Child c
-USING ( ChildID )
-LEFT JOIN EVT d
-USING ( ChildStudyID )
-LEFT JOIN PPVT e
-USING ( ChildStudyID )
-LEFT JOIN VerbalFluency f
-USING ( ChildStudyID )
-LEFT JOIN FruitStroop h
-USING ( ChildStudyID )
-WHERE a.Study =  "TimePoint2"
-ORDER BY b.ShortResearchID
+CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint2` AS
+  SELECT b.ChildStudyID, a.Study, b.ShortResearchID AS `ResearchID`,
+    c.Female, c.AAE, c.LateTalker, c.CImplant,
+    d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard,
+    e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard,
+    f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq,
+    h.FruitStroop_Completion, h.FruitStroop_Age, h.FruitStroop_Score,
+    i.MinPair_ProportionCorrect,
+    j.SAILS_Age, j.SAILS_NumPracticeTrials, j.SAILS_NumTestTrials, j.SAILS_ProportionTestCorrect
+  FROM Study a
+  LEFT JOIN ChildStudy b
+  USING ( StudyID )
+  LEFT JOIN Child c
+  USING ( ChildID )
+  LEFT JOIN EVT d
+  USING ( ChildStudyID )
+  LEFT JOIN PPVT e
+  USING ( ChildStudyID )
+  LEFT JOIN VerbalFluency f
+  USING ( ChildStudyID )
+  LEFT JOIN FruitStroop h
+  USING ( ChildStudyID )
+  LEFT JOIN q_MinPair_Aggregate i
+  USING ( ChildStudyID )
+  LEFT JOIN q_SAILS_Aggregate j
+  USING ( ChildStudyID )
+  WHERE a.Study = "TimePoint2"
+  ORDER BY b.ShortResearchID
