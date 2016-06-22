f6a61b880a1606befac785b95dfadadf50aae47f
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-06-22 14:12:05 -0500

    add SAILS queries

---
+++ inst/views/sails_propcorrect.sql
@@ -0,0 +1,19 @@
+--
+-- Create a view to display the proportion correct for non-training trials in
+-- each administration of the SAILS experiment.
+--
+
+CREATE ALGORITHM = UNDEFINED VIEW  `q_SAILS_PropCorrect` AS
+  SELECT d.ChildStudyID, c.Study, d.ShortResearchID AS `ResearchID`,
+    b.SAILS_EprimeFile, b.SAILS_Completion, b.SAILS_Dialect, b.SAILS_Age,
+    ROUND( AVG( a.Correct ) , 4.0 ) AS  `SAILS_ProportionCorrect`
+  FROM ChildStudy d
+  LEFT JOIN Study c
+  USING ( StudyID )
+  LEFT JOIN SAILS_Admin b
+  USING ( ChildStudyID )
+  LEFT JOIN SAILS_Responses a
+  USING ( SAILSID )
+  WHERE a.Running = "Test"
+  GROUP BY b.SAILSID
+  ORDER BY c.Study, d.ShortResearchID
