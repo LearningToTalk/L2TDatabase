212e8ba55f9c2821463bb89c5d6d7c0632581672
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-06-22 14:24:52 -0500

    add childstudy id to all views

--- inst/views/tp3_scores.sql
+++ inst/views/tp3_scores.sql
@@ -3,17 +3,28 @@
 -- Replace CREATE with ALTER to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint3` AS SELECT a.Study, b.ShortResearchID AS  `ResearchID` , c.Female, c.AAE, c.LateTalker, c.CImplant, d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard, e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard, f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq
-FROM Study a
-LEFT JOIN ChildStudy b
-USING ( StudyID )
-LEFT JOIN Child c
-USING ( ChildID )
-LEFT JOIN EVT d
-USING ( ChildStudyID )
-LEFT JOIN PPVT e
-USING ( ChildStudyID )
-LEFT JOIN VerbalFluency f
-USING ( ChildStudyID )
-WHERE a.Study =  "TimePoint3"
-ORDER BY b.ShortResearchID
+CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint3` AS
+  SELECT b.ChildStudyID, a.Study, b.ShortResearchID AS  `ResearchID`,
+    c.Female, c.AAE, c.LateTalker, c.CImplant,
+    d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard,
+    e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard,
+    f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq,
+    i.MinPair_ProportionCorrect,
+    j.SAILS_Age, j.SAILS_NumPracticeTrials, j.SAILS_NumTestTrials, j.SAILS_ProportionTestCorrect
+  FROM Study a
+  LEFT JOIN ChildStudy b
+  USING ( StudyID )
+  LEFT JOIN Child c
+  USING ( ChildID )
+  LEFT JOIN EVT d
+  USING ( ChildStudyID )
+  LEFT JOIN PPVT e
+  USING ( ChildStudyID )
+  LEFT JOIN VerbalFluency f
+  USING ( ChildStudyID )
+  LEFT JOIN q_MinPair_Aggregate i
+  USING ( ChildStudyID )
+  LEFT JOIN q_SAILS_Aggregate j
+  USING ( ChildStudyID )
+  WHERE a.Study = "TimePoint3"
+  ORDER BY b.ShortResearchID
