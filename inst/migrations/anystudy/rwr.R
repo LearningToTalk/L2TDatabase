852f6202eac80fa4fc00d36f0bba704012c2b16f
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-11-18 11:23:17 -0600

    bulk import RWR administration information

--- inst/migrations/anystudy/rwr.R
+++ inst/migrations/anystudy/rwr.R
@@ -9,45 +9,56 @@ tp1_rwr_files <- locate_files(
   l2t_eprime_data$rwr_file
 )
 
+tp2_rwr_files <- locate_files(
+  file.path(l2t_eprime_data$rwr_dir, "TimePoint2/Recordings"),
+  l2t_eprime_data$rwr_file
+)
+
+tp3_rwr_files <- locate_files(
+  file.path(l2t_eprime_data$rwr_dir, "TimePoint3/Recordings"),
+  l2t_eprime_data$rwr_file
+)
+
 try_load_rwr_file <- safely(get_rwr_trial_info)
 try_make_rwr_wordlist <- safely(lookup_rwr_wordlist)
 
-tp1_rwr_data <- tp1_rwr_files %>%
-  map(try_load_rwr_file)
+files <- list(
+  TimePoint1 = tp1_rwr_files %>% set_names(basename(.)),
+  TimePoint2 = tp2_rwr_files %>% set_names(basename(.)),
+  TimePoint3 = tp3_rwr_files %>% set_names(basename(.))
+)
 
-# Check any errors
-tp1_rwr_data %>%
-  setNames(basename(tp1_rwr_files)) %>%
-  map("error") %>%
-  compact
+# Load the data for each study
+rwr_data <- files %>%
+  at_depth(2, try_load_rwr_file)
 
-# Create word lists
-tp1_rwr_wordlists <- tp1_rwr_data %>%
-  map("result") %>%
-  map(try_make_rwr_wordlist)
+# Check each sublist for errors
+check_errors <- . %>% map("error") %>% compact
+rwr_data %>% map(check_errors)
 
-tp1_rwr_wordlists %>%
-  map("error") %>%
-  compact
+# Create wordlists for each sublist
+create_wordlist <- . %>% map("result") %>% map(try_make_rwr_wordlist)
+rwr_wordlists <- rwr_data %>%
+  map(create_wordlist)
 
-tp1_rwr_wordlists %>%
-  map_df("result") %>%
-  filter(is.na(TargetC))
+rwr_wordlists %>% map(check_errors)
 
 
 create_wide_wordlist <- function(x) {
   left_join(x, lookup_rwr_wordlist(x))
 }
 
-wide_word_lists <- tp1_rwr_data  %>%
+create_dataframe_for_study <- . %>%
   map("result") %>%
   map_df(create_wide_wordlist)
 
+wide_word_lists <- rwr_data %>%
+  map(create_dataframe_for_study) %>%
+  bind_rows(.id = "Study")
 
 wide_word_lists <- wide_word_lists %>%
   mutate(
-    ResearchID = stringr::str_extract(Eprime.Basename, "\\d\\d\\d\\w"),
-    Study = "TimePoint1") %>%
+    ResearchID = stringr::str_extract(Eprime.Basename, "\\d\\d\\d\\w")) %>%
   rename(RealWordRep_Experiment = Experiment,
          RealWordRep_TimePoint = TimePoint,
          RealWordRep_Dialect = Dialect,
@@ -55,13 +66,23 @@ wide_word_lists <- wide_word_lists %>%
          RealWordRep_Completion = Date,
          RealWordRep_Helper = Helper)
 
+
 administrations <- wide_word_lists %>%
   select(Study, ResearchID, starts_with("RealWordRep")) %>%
   distinct
 
 # Don't database participants who got the wrong version?
-administrations <- administrations %>%
-  filter(RealWordRep_TimePoint == 1)
+version_check <- tibble::tribble(
+  ~ Study,        ~ RealWordRep_TimePoint,
+  "TimePoint1",   1,
+  "TimePoint2",   2,
+  "TimePoint3",   3
+)
+
+wrong_block_given <- administrations %>% anti_join(version_check)
+wrong_block_given
+
+administrations <- administrations %>% anti_join(wrong_block_given)
 
 
 library(L2TDatabase)
@@ -82,7 +103,7 @@ administrations <- administrations %>%
   mutate(RealWordRep_Age = chrono_age(Birthdate, RealWordRep_Completion))
 
 administrations %>%
-  count(ResearchID) %>%
+  count(ResearchID, ChildStudyID) %>%
   filter(1 < n) %>%
   {stopifnot(nrow(.) == 0)}
 
@@ -90,22 +111,19 @@ remote_admins <- tbl(l2t, "RealWordRep_Admin") %>%
   collect %>%
   readr::type_convert()
 
-# Check for children not found in database
-local_admins <- match_columns(administrations, remote_admins)
-local_admins %>% filter(is.na(ChildStudyID))
-
-
 # Remove adminstrations already in database
-rows_to_add <- local_admins %>%
-  anti_join(remote_admins, by = c("ChildStudyID", "RealWordRep_EprimeFile")) %>%
-  arrange(ChildStudyID, RealWordRep_EprimeFile)
+rows_to_add <- find_new_rows_in_table(
+  data = administrations,
+  ref_data = remote_admins,
+  required_cols = c("ChildStudyID"),
+  extra_cols = c("RealWordRep_EprimeFile", "RealWordRep_Completion"))
 rows_to_add
 
 # Preview who is being added
 inner_join(administrations, readr::type_convert(rows_to_add)) %>%
   select(ChildStudyID, Study, ResearchID, RealWordRep_EprimeFile) %>%
-  as.data.frame %>%
-  arrange(Study, ResearchID)
+  arrange(Study, ResearchID) %>%
+  print(n = 100)
 
 # There should not be any repeated file names
 stopifnot(length(rows_to_add$RealWordRep_EprimeFile) == n_distinct(rows_to_add$RealWordRep_EprimeFile))
