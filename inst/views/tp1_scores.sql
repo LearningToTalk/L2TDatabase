ba2920c2cec45677b963d6d328ef58ddcb73965a
Tristan Jay Mahr <Mahr@Wisc.Edu>
2016-12-12 13:09:20 -0600

    update queries to include medu

--- inst/views/tp1_scores.sql
+++ inst/views/tp1_scores.sql
@@ -1,36 +1,73 @@
 --
 -- Create a view to display the test scores of TimePoint1 participants
--- Replace CREATE with ALTER to update an existing view
+-- Replace "create" with "alter" to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint1` AS
-  SELECT b.ChildStudyID, a.Study, b.ShortResearchID AS `ResearchID`,
-    c.Female, c.AAE, c.LateTalker, c.CImplant,
-    d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard,
-    e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard,
-    f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq,
-    g.GFTA_Completion, g.GFTA_RawCorrect, g.GFTA_NumTranscribed, g.GFTA_AdjCorrect, g.GFTA_Standard,
-    h.FruitStroop_Completion, h.FruitStroop_Age, h.FruitStroop_Score,
-    i.MinPair_Age, i.MinPair_ProportionCorrect,
-    j.RealWordRep_Completion, j.RealWordRep_Experiment, j.RealWordRep_Age
-  FROM Study a
-  LEFT JOIN ChildStudy b
-  USING ( StudyID )
-  LEFT JOIN Child c
-  USING ( ChildID )
-  LEFT JOIN EVT d
-  USING ( ChildStudyID )
-  LEFT JOIN PPVT e
-  USING ( ChildStudyID )
-  LEFT JOIN VerbalFluency f
-  USING ( ChildStudyID )
-  LEFT JOIN GFTA g
-  USING ( ChildStudyID )
-  LEFT JOIN FruitStroop h
-  USING ( ChildStudyID )
-  LEFT JOIN q_MinPair_Aggregate i
-  USING ( ChildStudyID )
-  LEFT JOIN RealWordRep_Admin j
-  USING ( ChildStudyID )
-  WHERE a.Study = "TimePoint1"
-  ORDER BY b.ShortResearchID
+create algorithm = undefined view  `q_Scores_TimePoint1` as
+  select
+    childstudy.ChildStudyID,
+    child.HouseholdID,
+    study.Study,
+    childstudy.ShortResearchID as `ResearchID`,
+    child.Female,
+    child.AAE,
+    child.LateTalker,
+    child.CImplant,
+    medu.Caregiver_Relation as `Maternal_Caregiver`,
+    medu.Caregiver_EduCategory as `Maternal_Education`,
+    medu.Caregiver_EduScale as `Maternal_Education_Level`,
+    evt.EVT_Completion,
+    evt.EVT_Form,
+    evt.EVT_Raw,
+    evt.EVT_GSV,
+    evt.EVT_Age,
+    evt.EVT_Standard,
+    ppvt.PPVT_Completion,
+    ppvt.PPVT_Form,
+    ppvt.PPVT_Raw,
+    ppvt.PPVT_GSV,
+    ppvt.PPVT_Age,
+    ppvt.PPVT_Standard,
+    vf.VerbalFluency_Completion,
+    vf.VerbalFluency_Age,
+    vf.VerbalFluency_Raw,
+    vf.VerbalFluency_AgeEq,
+    gfta.GFTA_Completion,
+    gfta.GFTA_RawCorrect,
+    gfta.GFTA_NumTranscribed,
+    gfta.GFTA_AdjCorrect,
+    gfta.GFTA_Standard,
+    fruit.FruitStroop_Completion,
+    fruit.FruitStroop_Age,
+    fruit.FruitStroop_Score,
+    minpair.MinPair_Age,
+    minpair.MinPair_ProportionCorrect,
+    rwr.RealWordRep_Completion,
+    rwr.RealWordRep_Experiment,
+    rwr.RealWordRep_Age
+  from
+    Study study
+    left join ChildStudy childstudy
+      using (StudyID)
+    left join Child child
+      using (ChildID)
+    left join q_Household_Max_Maternal_Education medu
+      using (HouseholdID)
+    left join EVT evt
+      using (ChildStudyID)
+    left join PPVT ppvt
+      using (ChildStudyID)
+    left join VerbalFluency vf
+      using (ChildStudyID)
+    left join GFTA gfta
+      using (ChildStudyID)
+    left join FruitStroop fruit
+      using (ChildStudyID)
+    left join q_MinPair_Aggregate minpair
+      using (ChildStudyID)
+    left join RealWordRep_Admin rwr
+      using (ChildStudyID)
+  where
+    study.Study = "TimePoint1"
+  order by
+    childstudy.ShortResearchID
--- inst/views/tp3_scores.sql
+++ inst/views/tp3_scores.sql
@@ -1,33 +1,66 @@
 --
 -- Create a view to display the test scores of TimePoint3 participants
--- Replace CREATE with ALTER to update an existing view
+-- Replace "create" with "alter" to update an existing view
 --
 
-CREATE ALGORITHM = UNDEFINED VIEW  `q_Scores_TimePoint3` AS
-  SELECT b.ChildStudyID, a.Study, b.ShortResearchID AS  `ResearchID`,
-    c.Female, c.AAE, c.LateTalker, c.CImplant,
-    d.EVT_Completion, d.EVT_Form, d.EVT_Raw, d.EVT_GSV, d.EVT_Age, d.EVT_Standard,
-    e.PPVT_Completion, e.PPVT_Form, e.PPVT_Raw, e.PPVT_GSV, e.PPVT_Age, e.PPVT_Standard,
-    f.VerbalFluency_Completion, f.VerbalFluency_Age, f.VerbalFluency_Raw, f.VerbalFluency_AgeEq,
-    i.MinPair_Age, i.MinPair_ProportionCorrect,
-    j.SAILS_Age, j.SAILS_NumPracticeTrials, j.SAILS_NumTestTrials, j.SAILS_ProportionTestCorrect,
-    k.RealWordRep_Completion, k.RealWordRep_Experiment, k.RealWordRep_Age
-  FROM Study a
-  LEFT JOIN ChildStudy b
-  USING ( StudyID )
-  LEFT JOIN Child c
-  USING ( ChildID )
-  LEFT JOIN EVT d
-  USING ( ChildStudyID )
-  LEFT JOIN PPVT e
-  USING ( ChildStudyID )
-  LEFT JOIN VerbalFluency f
-  USING ( ChildStudyID )
-  LEFT JOIN q_MinPair_Aggregate i
-  USING ( ChildStudyID )
-  LEFT JOIN q_SAILS_Aggregate j
-  USING ( ChildStudyID )
-  LEFT JOIN RealWordRep_Admin k
-  USING ( ChildStudyID )
-  WHERE a.Study = "TimePoint3"
-  ORDER BY b.ShortResearchID
+create algorithm = undefined view  `q_Scores_TimePoint3` as
+  select
+    childstudy.ChildStudyID,
+    study.Study,
+    childstudy.ShortResearchID as `ResearchID`,
+    child.Female,
+    child.AAE,
+    child.LateTalker,
+    child.CImplant,
+    medu.Caregiver_Relation as `Maternal_Caregiver`,
+    medu.Caregiver_EduCategory as `Maternal_Education`,
+    medu.Caregiver_EduScale as `Maternal_Education_Level`,
+    evt.EVT_Completion,
+    evt.EVT_Form,
+    evt.EVT_Raw,
+    evt.EVT_GSV,
+    evt.EVT_Age,
+    evt.EVT_Standard,
+    ppvt.PPVT_Completion,
+    ppvt.PPVT_Form,
+    ppvt.PPVT_Raw,
+    ppvt.PPVT_GSV,
+    ppvt.PPVT_Age,
+    ppvt.PPVT_Standard,
+    vf.VerbalFluency_Completion,
+    vf.VerbalFluency_Age,
+    vf.VerbalFluency_Raw,
+    vf.VerbalFluency_AgeEq,
+    minpair.MinPair_Age,
+    minpair.MinPair_ProportionCorrect,
+    sails.SAILS_Age,
+    sails.SAILS_NumPracticeTrials,
+    sails.SAILS_NumTestTrials,
+    sails.SAILS_ProportionTestCorrect,
+    rwr.RealWordRep_Completion,
+    rwr.RealWordRep_Experiment,
+    rwr.RealWordRep_Age
+  from
+    Study study
+    left join ChildStudy childstudy
+      using (StudyID)
+    left join Child child
+      using (ChildID)
+    left join q_Household_Max_Maternal_Education medu
+      using (HouseholdID)
+    left join EVT evt
+      using (ChildStudyID)
+    left join PPVT ppvt
+      using (ChildStudyID)
+    left join VerbalFluency vf
+      using (ChildStudyID)
+    left join q_MinPair_Aggregate minpair
+      using (ChildStudyID)
+    left join q_SAILS_Aggregate sails
+      using (ChildStudyID)
+    left join RealWordRep_Admin rwr
+      using (ChildStudyID)
+  where
+    study.Study = "TimePoint3"
+  order by
+    childstudy.ShortResearchID
