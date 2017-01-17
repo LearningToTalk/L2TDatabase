d8e934ba00ed48e7681f865820a3ab8ca7e93fe4
Tristan Jay Mahr <Mahr@Wisc.Edu>
2017-01-17 14:08:19 -0600

    add ctopp memory norms to database

--- inst/migrations/norms/ctopp.R
+++ inst/migrations/norms/ctopp.R
@@ -17,18 +17,27 @@ norm_db <- l2t_connect(cnf_file, "norms")
 elision_rows <- norms %>% filter(Subtest == "Elision") %>% select(-Subtest)
 blending_rows <- norms %>% filter(Subtest == "Blending") %>% select(-Subtest)
 
+memory_rows <- norms %>%
+  filter(Subtest == "Memory") %>%
+  select(-Subtest, -AgeEq, -GradeEq)
+
 # Make sure only new data is being added
-elision_current_rows <- tbl(norm_db, "CTOPP_Elision") %>% collect
+elision_current_rows <- tbl(norm_db, "CTOPP_Elision") %>% collect()
 elision_new_rows <- anti_join(elision_rows, elision_current_rows) %>%
   arrange(Age, Raw)
 
-blending_current_rows <- tbl(norm_db, "CTOPP_Blending") %>% collect
+blending_current_rows <- tbl(norm_db, "CTOPP_Blending") %>% collect()
 blending_new_rows <- anti_join(blending_rows, blending_current_rows) %>%
+  arrange(Age, Raw)
+
+memory_current_rows <- tbl(norm_db, "CTOPP_Memory") %>% collect()
+memory_new_rows <- anti_join(memory_rows, memory_current_rows) %>%
   arrange(Age, Raw)
 
 # Add to database
 append_rows_to_table(norm_db, "CTOPP_Blending", blending_new_rows)
 append_rows_to_table(norm_db, "CTOPP_Elision", elision_new_rows)
+append_rows_to_table(norm_db, "CTOPP_Memory", memory_new_rows)
 
 # Check that remote_rows minus local_rows is empty
 current_rows <- tbl(norm_db, "CTOPP_Blending") %>% collect
@@ -37,6 +46,10 @@ leftover_rows
 
 current_rows <- tbl(norm_db, "CTOPP_Elision") %>% collect
 leftover_rows <- anti_join(elision_rows, current_rows)
+leftover_rows
+
+current_rows <- tbl(norm_db, "CTOPP_Memory") %>% collect
+leftover_rows <- anti_join(memory_rows, current_rows)
 leftover_rows
 
 ## Demos of how to use the norms
